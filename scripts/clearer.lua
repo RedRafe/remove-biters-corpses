1dcfb626dbff4b7e4c9bade207821c5998b4cfb6
RedRafe <gabri97.gt+redrafe@gmail.com>
2025-01-01 16:14:02 +0100

    release v2.0.3

--- scripts/clearer.lua
+++ scripts/clearer.lua
@@ -1,6 +1,8 @@
 local blacklist = {
-  'character-corpse',
-  'transport-caution-corpse'  -- Transport Drones
+  ['character-corpse'] = true,
+  ['transport-caution-corpse'] = true,  -- Transport Drones
+  ['spidertron-enhancements-corpse'] = true, -- Spidertron Enhancements
+  ['spidertron-remnants'] = true, -- Spidertron Enhancements
 }
 
 local YELLOW = { r = 254/255, g = 255/255, b = 10/255, a = 1}
@@ -8,33 +10,22 @@ local GREEN = { r = 49/255, g = 190/255, b = 48/255, a = 1}
 
 --=================================================================================================
 
--- @ tableIn: Table
--- @ element: any
-local function tableContains(tableIn, element)
-  for ___, value in pairs(tableIn) do
-    if value == element then
-      return true
-    end
-  end
-  return false
-end
-
---=================================================================================================
-
 -- @ event: CustomInputEvent
 -- Docs: https://lua-api.factorio.com/latest/events.html#CustomInputEvent
 local function removeCorpses(event)
-  local player = game.players[event.player_index]
-  if not player then return end
+  local player = game.get_player(event.player_index)
+  if not player then
+    return
+  end
   local radius = settings.get_player_settings(event.player_index)['rbc-radius'].value
   
   local bodyCount = 0
   for ___, entity in pairs(player.surface.find_entities_filtered{
     position = player.position,
     radius = radius,
-    type = 'corpse'}
-  ) do 
-    if not tableContains(blacklist, entity.name) then 
+    type = 'corpse'
+  }) do 
+    if not blacklist[entity.name] then 
       --game.print(entity.name)
       entity.destroy()
       bodyCount = bodyCount + 1
